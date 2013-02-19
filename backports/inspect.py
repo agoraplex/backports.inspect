ffba91fb9590e942c80e2570f5a5d044102702bf
Tripp Lilley <tripplilley@gmail.com>
2013-02-19 09:37:54 -0500

    *actually* fix the monkeypatched `formatannotation`
    ...instead of just replacing the original in `funcsigs` with
    *an exact copy/paste of itself!*. Doh!!

--- backports/inspect.py
+++ backports/inspect.py
@@ -16,7 +16,7 @@ from funcsigs import *
 # integrated into :mod:`funcsigs`.
 def formatannotation(annotation, base_module=None):
     if isinstance(annotation, type):
-        if annotation.__module__ in ('builtins', base_module):
+        if annotation.__module__ in ('builtins', '__builtin__', base_module):
             return annotation.__name__
         return annotation.__module__+'.'+annotation.__name__
     return repr(annotation)
