1c42a825de6631a1b48fceb252903fed3fc1ce25
Tripp Lilley <tripplilley@gmail.com>
2013-02-19 07:12:05 -0500

    remove confusing (to me) `distribute_setup` bootstrap

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-from distribute_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup
-
 
 setup(
     name='backports',
@@ -15,6 +11,6 @@ setup(
     license='See LICENSE',
     description='',
     long_description=open('README.rst').read(),
-    install_requires=['funcsigs>=0.2'],
+    install_requires=['distribute', 'funcsigs>=0.2'],
     tests_require=['nose>=1.2.1'],
 )
