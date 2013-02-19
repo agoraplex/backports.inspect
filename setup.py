25c0b756961ca11e343251f6858125776d6551da
Tripp Lilley <tripplilley@gmail.com>
2013-02-19 08:04:14 -0500

    clean up and flesh out setup metadata

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup
 
 setup(
-    name='backports',
+    name='backports.inspect',
     version='0.0.0',
+
     author='Tripp Lilley',
     author_email='tripplilley@gmail.com',
-    packages=['backports.inspect'],
-    namespace_packages=['backports'],
-    url='',
-    license='See LICENSE',
-    description='',
+    url='https://github.com/agoraplex/backports.inspect',
+
+    license='BSD',
+    description='Backport (some) Python 3 `inspect` module features to 2.7 (extending aliles/funcsigs)',
     long_description=open('README.rst').read(),
-    install_requires=['distribute', 'funcsigs==0.2'],
-    tests_require=['nose>=1.2.1'],
+    packages=[
+        'backports.inspect',
+        ],
+    namespace_packages=[
+        'backports',
+        ],
+
+    install_requires=[
+        'distribute',
+        'funcsigs==0.2',
+        ],
+    tests_require=[
+        'nose>=1.2.1',
+        ],
 )
